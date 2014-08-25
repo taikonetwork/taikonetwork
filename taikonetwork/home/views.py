9e5059c4cb6e8ecc852c1d6e77f23ffc2f84277d
Kenny Ng <kennyng.zq@gmail.com>
2014-08-25 10:03:01 +0000

    Updated home pages.

--- taikonetwork/home/views.py
+++ taikonetwork/home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'login/index.html')
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
--- taikonetwork/taikonetwork/urls.py
+++ taikonetwork/taikonetwork/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
+import home.views
+
 urlpatterns = patterns('',
-    url(r'^$', include('login.urls')),
+    url(r'^$', home.views.index, name='index'),
+    url(r'^about/', home.views.about, name='about'),
     url(r'^admin/', include(admin.site.urls)),
 )
