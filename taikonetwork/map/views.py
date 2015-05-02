e2b45965778736cab9ebbae59a830c7f90ff5e26
Kenny Ng <kennyng.zq@gmail.com>
2015-05-02 09:08:11 +0000

    Cache groupmap and grouplist json data.

--- taikonetwork/map/views.py
+++ taikonetwork/map/views.py
@@ -2,93 +2,110 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.core.serializers.json import DjangoJSONEncoder
+from django.core.cache import cache
 
 import json
 from datetime import datetime
 
-from datahandler.models import GroupAlt
-from py2neo import cypher
-from taikonetwork.neo4j_settings import NEO4J_ROOT_URI
+from datahandler.models import Group
 
 
 @login_required(login_url='/')
 def groupmap(request):
-    taiko_groups = GroupAlt.objects.filter(accounttype='Taiko Group')
-    groups_filtered = []
-    num_no_loc = 0
-    for g in taiko_groups:
-        if g.latitude and g.longitude:
-            year = None
-            if g.founding_date:
-                year = g.founding_date.year
+    cache_key_json = 'map_groupmap_json'
+    cache_key_count = 'map_groupmap_num_no_loc'
+    # Store in cache for 1 week (in seconds)
+    cache_timeout = 604800
+    groups_json = cache.get(cache_key_json)
+    num_no_loc = cache.get(cache_key_count)
 
-            group = {'name': g.name, 'sf_id': g.sf_id, 'year': year,
-                     'lng': g.longitude, 'lat': g.latitude}
-            groups_filtered.append(group)
-        else:
-            num_no_loc += 1
+    if not groups_json or not num_no_loc:
+        taiko_groups = Group.objects.all()
+        groups_filtered = []
+        num_no_loc = 0
+        for g in taiko_groups:
+            if g.latitude and g.longitude:
+                year = '----'
+                if g.founding_date:
+                    year = g.founding_date.year
 
-    groups_json = json.dumps(groups_filtered, cls=DjangoJSONEncoder)
+                group_info = {}
+                if g.website:
+                    group_info['website'] = g.website
+                if g.city or g.state or g.country:
+                    location = '{0}, {1}, {2}'.format(g.city, g.state, g.country)
+                    location = location.replace(' , ', ' ')
+                    location = location.lstrip(', ')
+                    location = location.rstrip(', ')
+                    group_info['location'] = location
+
+                group = {'name': g.name, 'sf_id': g.sf_id, 'year': year,
+                         'lng': g.longitude, 'lat': g.latitude,
+                         'info': group_info}
+                groups_filtered.append(group)
+            else:
+                num_no_loc += 1
+
+        groups_json = json.dumps(groups_filtered, cls=DjangoJSONEncoder)
+        cache.set(cache_key_json, groups_json, cache_timeout)
+        cache.set(cache_key_count, num_no_loc, cache_timeout)
+
     return render(request, 'map/groupmap.html', {'groups_json': groups_json,
                                                  'num_no_loc': num_no_loc})
 
 
 @login_required(login_url='/')
 def grouplist(request):
-    taiko_groups = GroupAlt.objects.filter(accounttype='Taiko Group')
-    groups_filtered = []
-    for g in taiko_groups:
-        year = None
-        if g.founding_date:
-            year = g.founding_date.year
+    cache_key = 'map_grouplist_json'
+    # Store in cache for 1 week (in seconds)
+    cache_timeout = 604800
+    groups_json = cache.get(cache_key)
 
-        groups_filtered.append({'name': g.name, 'category': g.category,
-                                'website': g.website,
-                                'year': year, 'country': g.country,
-                                'state': g.state, 'city': g.city})
+    if not groups_json:
+        taiko_groups = Group.objects.all()
+        groups_filtered = []
+        for g in taiko_groups:
+            year = None
+            if g.founding_date:
+                year = g.founding_date.year
+            groups_filtered.append({'name': g.name, 'category': g.category,
+                                    'website': g.website,
+                                    'year': year, 'country': g.country,
+                                    'state': g.state, 'city': g.city})
 
-    groups_json = json.dumps(groups_filtered, cls=DjangoJSONEncoder)
+        groups_json = json.dumps(groups_filtered, cls=DjangoJSONEncoder)
+        cache.set(cache_key, groups_json, cache_timeout)
+
     return render(request, 'map/grouplist.html', {'groups_json': groups_json})
 
 
 def query_members(request):
-    query_str = ('MATCH (m:Member)-[r:MEMBER_OF]->'
-                 '(g:Group {{ name: "{group}", sf_id: "{sf_id}" }}) '
-                 'WHERE r.start <= toInt("{end_year}") '
-                 'RETURN m.firstname + " " + m.lastname, r.start, r.end;')
-    group = request.GET.get('group', '')
+    group_name = request.GET.get('group', '')
     sf_id = request.GET.get('sf_id', '')
     end_year = request.GET.get('end_year', '')
-    query = query_str.format(group=group, sf_id=sf_id, end_year=end_year)
+    enddate = '{0}-12-31'.format(end_year)
 
     try:
-        session = cypher.Session(NEO4J_ROOT_URI)
-        tx = session.create_transaction()
-        tx.append(query)
+        group = Group.objects.get(sf_id=sf_id, name=group_name)
+        memberships = group.memberships.filter(startdate__lte=enddate)
+    except Group.DoesNotExist:
+        return HttpResponse({'status': 'error', 'msg': 'Taiko Group Not Found.'})
 
-        # Execute all queries on server and commit transaction.
-        result = tx.commit()
-    except cypher.TransactionError as error:
-        return HttpResponse({'status': 'error', 'msg': str(error)})
-    else:
-        if tx.finished:
-            members = []
-            if len(result[0]) > 0:
-                for m in result[0]:
-                    start = m[1]
-                    end = m[2]
-                    current = datetime.now().year
-                    if start == 9999:
-                        start = current
-                    if end == 9999:
-                        end = current
-                    year = start
-                    if start != end:
-                        year = '{} - {}'.format(start, end)
-                    members.append({'name': m[0], 'active_years': year})
-
-            response = json.dumps({'status': 'ok', 'members': members, 'query': query}, cls=DjangoJSONEncoder)
-            return HttpResponse(response, content_type="application/json")
+    members = []
+    for mship in memberships:
+        current = datetime.now().year
+        start = mship.startdate.year
+        if mship.enddate is None:
+            if mship.status == 'Current':
+                end = current
+            else:
+                end = start
         else:
-            return HttpResponse({'status': 'error', 'msg':
-                                           'NEO4J ERROR: Transaction not finished.'})
+            end = mship.enddate.year
+        year = start
+        if start != end:
+            year = '{0} - {1}'.format(start, end)
+        members.append({'name': mship.member.name, 'active_years': year})
+
+    response = json.dumps({'status': 'ok', 'members': members}, cls=DjangoJSONEncoder)
+    return HttpResponse(response, content_type="application/json")
